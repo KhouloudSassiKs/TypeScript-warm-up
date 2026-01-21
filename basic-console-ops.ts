c7fff09012be32896621367644d90e0c49ae7c15
Khouloud <sassikhouloud45@gmail.com>
2026-01-21 18:52:13 +0100

    Create basic-console-ops.ts

---
+++ basic-console-ops.ts
@@ -0,0 +1,48 @@
+// basic print
+console.log("Hello, World!");
+
+// Defining a variable
+let x = 20;
+
+// Print the variable's value to the console
+console.log(x); // Outputs: 20
+
+let age = 25;
+console.log("Assigning the age variable:", age);
+
+// Print a celebratory message
+console.log("Yaaay! I did it!");
+
+let hieroglyph: number = 1290; // A numerical value found
+let yearDiscovered: string = "2022 A.D."; // The year it was discovered
+
+console.log(
+  "The hieroglyph discovered in",
+  yearDiscovered,
+  "translates to the year",
+  hieroglyph,
+  "B.C. in the cosmic calendar."
+);
+
+
+let wheatHarvested: number = 3000;   // Amount of wheat harvested in cosmic units
+let barleyHarvested: number = 2000;  // Amount of barley harvested in cosmic units
+let cosmicCornHarvested: number = 4000; // Amount of cosmic corn harvested
+
+console.log(
+  "We have harvested",
+  wheatHarvested,
+  "cosmic units of wheat,",
+  barleyHarvested,
+  "cosmic units of barley, and",
+  cosmicCornHarvested,
+  "cosmic units of corn this cycle."
+);
+
+// We need to highlight another legendary Samurai in our virtual exhibit
+// Prepare the necessary variables and utilize console.log to complete the code
+
+let samurai: string = "Samurai Miyamoto Musashi"; 
+let era: string = "Edo period"; // Samurai Miyamoto Musashi was active during the Edo period
+
+console.log(samurai, "lived during the", era);
